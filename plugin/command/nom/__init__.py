6d6f74adfb6fb2e08063b7a1c42cc7bb83b24f1c
sviik <sviik@aloev.era.ee>
2014-12-13 17:39:59 +0200

    nom

--- plugin/command/nom/__init__.py
+++ plugin/command/nom/__init__.py
@@ -16,12 +16,6 @@ def getResponseType():
 
 def get(parameter, channel, author, folder):
     result = []
-    link = re.search(noirRe, urlopen("http://www.cafenoir.ee").read())
-    if (link):
-        noirNom = re.search(noirArticleRe, urlopen("http://www.cafenoir.ee" + link.group(1)).read())
-        if (noirNom):
-            result.append("NOIR: " + noirNom.group(1).strip(' \t\n\r') + "; " + noirNom.group(3).strip(' \t\n\r') + " (" + noirNom.group(2).strip(' \t\n\r') + ")")
-
     matches = re.findall(paevapraedRe, urlopen("http://www.paevapraed.com").read())
     if (matches):
         for match in matches:
